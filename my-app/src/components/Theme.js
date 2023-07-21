c75763ed5a111d19fdf37b36b608c5648b887038
Rahul Bera <rahaulbera3654@gmail.com>
2023-07-21 17:10:49 +0530

    first commit

---
+++ my-app/src/components/Theme.js
@@ -0,0 +1,12 @@
+// theme.js
+const theme = {
+    colors: {
+      primary: "#4285f4", // Blue
+      secondary: "#333333", // Dark Gray
+      accent: "#ff5722", // Orange
+      background: "#f2f2f2", // Light Gray
+    },
+  };
+  
+  export default theme;
+  
